d6696dcb9636bcf08aab284c55f6b0ac7c11c1b5
ibrahim Keskin <brhmkskn04@gmail.com>
2021-07-30 00:48:07 +0300

    Add Product Service and Observable and subscribe details

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,23 +12,27 @@ import { HttpClient } from '@angular/common/http';
 export class ProductComponent implements OnInit {
 
   products: Product[] = [];
-  apiUrl = "https://localhost:44314/api/products/getall";
-  
+  dataLoaded=false;
   // yukarıda import ettiğimiz HttpClient ı constructor da inject ediyoruz. 
   //buradan inject ettiğimiz class sayfada tanımlı olmuş olur ve istediğimiz yerde this ile çağırıp kullanırız.
-  constructor(private httpClient: HttpClient) { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
     console.log("init çalıştı");
     this.getProducts();
   }
-
   getProducts() {
-    //fonksiyonlarda class içindeki bir parametreyi çağırırken this i kullanırız.
-    this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
-      .subscribe((response) => {
-        this.products=response.data
-       });
+    console.log("Api Başladı");
+    //subscribe sekron olarak datayı çekmeye devam eder. JS asekron olarak devam eder. 
+    //fakat subscribe içinde tüm işlemler bitip datalar çekildikten sonra işlem yapabiliriz.  
+   this.productService.getProducts().subscribe(response=>{
+     this.products=response.data
+
+     this.dataLoaded=true;
+     console.log("Api Bitti");
+   });
+   
+   console.log("Method Bitti");
   }
+  
 }
