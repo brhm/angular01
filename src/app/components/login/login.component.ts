7fc1a059a6eb312dec094cd86ead08c5c9196f12
ibrahim Keskin <brhmkskn04@gmail.com>
2021-08-01 19:33:26 +0300

    get token and add interceptors for request token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,23 +10,42 @@ import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService,
+    private toastrService:ToastrService,private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
   }
-  createLoginForm()
-  {
-    this.loginForm=this.formBuilder.group({
-      email:["",Validators.required],
-      password:["",Validators.required]
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
     })
 
   }
-  login(){
-    if(this.loginForm.valid){
+  login() {
+    if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      let loginModel = Object.assign({}, this.loginForm.value);
+      this.authService.login(loginModel).subscribe(response=>{
+        
+        this.toastrService.info(response.message);
+        
+        this.localStorageService.set("token",response.data.token)
+        
+        console.log(response);
+      },responseError=>{
+        console.log(responseError);
+        this.toastrService.error(responseError.error);
+      });
+    }
+  }
+  isAuthenticated() {
+    if (this.localStorageService.get("token")) {
+      return true;
+    } else {
+      return false;
     }
   }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { LoginModel } from '../models/loginModel';
+import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +14,9 @@ export class AuthService {
   apiUrl="https://localhost:44314/api/auth/";
   constructor(private httpClient:HttpClient) { }
 
-  login(loginModel:LoginModel)
+  login(loginModel:LoginModel):Observable<SingleResponseModel<TokenModel>>
   {
-    return this.httpClient.post(this.apiUrl+"login",loginModel)
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login",loginModel)
   }
-  
+
 }
