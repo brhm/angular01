3c7f00a1424b84013f076dd6b75fb57c02189b5d
ibrahim Keskin <brhmkskn04@gmail.com>
2021-08-01 16:07:03 +0300

    add persistent and localsotage

--- src/app/components/cart-summary/cart-summary.component.ts
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -3,6 +3,9 @@ import { ToastrService } from 'ngx-toastr';
 import { CartItem } from 'src/app/models/cartItem';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart-summary',
@@ -14,18 +17,25 @@ export class CartSummaryComponent implements OnInit {
   cartItems:CartItem[]=[];
   
 
-  constructor(private cartService:CartService,private toastrService:ToastrService) { }
+  constructor(private cartService:CartService,private toastrService:ToastrService,private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
+    
   this.getCart();
   }
 
   getCart(){
+
+    
     this.cartItems= this.cartService.list();
+    this.persist("cart",this.cartItems);
   }
   removeFromCart(product:Product)
   {
     this.cartService.removeFromCart(product);
     this.toastrService.error("Silindi",product.productName +" sepetten silindi.");
+  }
+  persist(key: string, value: any) {
+    this.localStorageService.set(key, value);
   }
 }
